fd9e7327155cabc24080638096ec9acc94b5c062
Another One <159118983+AnotherOne661@users.noreply.github.com>
2024-10-03 17:54:59 +0200

    Update main.js

--- subpages/leap-year/assets/js/main.js
+++ subpages/leap-year/assets/js/main.js
@@ -9,11 +9,10 @@ function calcularBisiesto(numero){
   num = parseInt(numero)
   console.log(num)
   const an = bis(numero)
-  if (num === 0){
+  if (num == 0){
     text.textContent = "No existe un año cero"
     return
   }
-  const an = bis(numero)
   if (an) {
     text.textContent = "Año bisiesto"
   } else {
